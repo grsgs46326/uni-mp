33567b36f149c3f1a46e7689660e37ff25d10d05
王小倩 <12933008+wwwxxx-qqq@user.noreply.gitee.com>
2023-11-18 21:57:18 +0800

    完成分类模块、地址模块、SUK模块

---
+++ src/api/login.ts
@@ -0,0 +1,42 @@
+/**
+ * 小程序登录
+ * @param data 请求参数
+ */
+export const postLoginWxMinAPI = (data: LoginParams) => {
+  return http<LoginResult>({
+    method: 'POST',
+    url: '/user/login/wxMin',
+    data
+  })
+}
+
+/**
+ * 小程序登录_内测版
+ * @param phoneNumber 模拟手机号码
+ */
+export const postLoginWxMinSimpleAPI = (phoneNumber: string) => {
+  return http<LoginResult>({
+    method: 'POST',
+    url: '/user/login/wxMin/simple',
+    data: {
+      phoneNumber
+    }
+  })
+}
+
+/**
+ * 获取个⼈信息
+ */
+export const getMemberProfileAPI = () => {
+  return http<ProfileDetail>({
+    method: 'GET',
+    url: '/user/profile'
+  })
+}
+export const putMemberProfileAPI = (data: ProfileParams) => {
+  return http<ProfileDetail>({
+    method: 'PUT',
+    url: '/user/profile',
+    data
+  })
+}
